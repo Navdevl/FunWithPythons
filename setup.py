b8522c914aa2e74bf5285678b58eef0dd9943266
Nav_devl <Naveendurai@users.noreply.github.com>
2016-12-19 03:53:40 +0530

    First Time With Setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='Final',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests']),
+    entry_points = {
+        'console_scripts': [
+            'magic = alohomora.cli:magic',
+            'longest = alohomora.cli:longest',
+        ]
+    },
+    install_requires=(['click','pytest']),
+    url='',
+    license='Still Nothing',
+    author='Naveen',
+    author_email='naveendurai19@gmail.com',
+    description='Trying to impress Skript :P'
+)
