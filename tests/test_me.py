2f9de69cf043c7d55e9e3dcce707a057cb6da41b
Nav_devl <Naveendurai@users.noreply.github.com>
2016-12-25 00:32:33 +0530

    Update test_me.py

--- tests/test_me.py
+++ tests/test_me.py
@@ -7,19 +7,19 @@ import pytest
 # ------------------------------------------------
 # Checking with Manually Computed Output
 def test_no_blank_words_true():
-    fun = Fun()
-    assert(fun.magic("edzlatsh", "hazel") == 1)
+    fun = Fun("edzlatsh", "hazel")
+    assert(fun.magic() == 1)
 
 def test_no_blank_words_false():
-    fun = Fun()
-    assert(fun.magic("edzlatsh", "hazely") == 0)
+    fun = Fun("edzlatsh", "hazely")
+    assert(fun.magic() == 0)
 # -------------------------------------------------
 
 # Checking if I did missed giving proper inputs in MAGIC
 def test_single_input_for_magic():
-    fun = Fun()
+    fun = Fun('Hey')
     with pytest.raises(ValueError):
-        fun.magic('Hey')
+        fun.magic()
 
 # Checking if I did missed giving proper inputs in LONGEST
 def test_no_input_for_longest():
