e94a0fbadc07bcdd5a6cd8d50b3b9792285eecfe
Nav_devl <Naveendurai@users.noreply.github.com>
2016-12-25 00:17:47 +0530

    Update __init__.py

--- alohomora/cli/__init__.py
+++ alohomora/cli/__init__.py
@@ -8,17 +8,18 @@ from alohomora import Alohomora as Fun
 @click.option('-s', default=None, help='Collection of letters. ')
 @click.option('-w', default=None, help='Word to compare all the letters in it with the already given one. ')
 def magic(s, w):
-    fun = Fun()
-    flag = fun.magic(s, w)
+    fun = Fun(s, w)
+    flag = fun.magic()
     if flag:
         print "Yes, \"" + w + "\" can be created."
     else:
         print "No, \"" + w + "\" cannot be created."
 
+
 @click.command()
 @click.option('-s', default=None, help='Collection of stupid letters.')
 def longest(s):
-    fun = Fun()
-    print fun.longest(s)
+    fun = Fun(s)
+    print fun.longest()
 
 
